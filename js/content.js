8c44867b59af4e01e4f8a3b08abec6fea5ffbc31
Can ÇALIŞKAN <cancaliskan@windowslive.com>
2020-04-12 14:33:03 +0300

    initial commit

---
+++ js/content.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+    $('.currency section').addClass('loading');
+    $.getJSON({ url: "http://dovizapi.cancaliskan.me" }).done(data => {
+        if (data) {
+            $(".currency .message").hide();
+            $(".currency #dollarBuyingPrice").text(data.Dollar.BuyingPrice);
+            $(".currency #dollarSellingPrice").text(data.Dollar.SellingPrice);
+            $(".currency #dollarUpdateTime").text(data.Dollar.UpdateTime);
+
+            $(".currency #euroBuyingPrice").text(data.Euro.BuyingPrice);
+            $(".currency #euroSellingPrice").text(data.Euro.SellingPrice);
+            $(".currency #euroUpdateTime").text(data.Euro.UpdateTime);
+
+            $(".currency #gramGoldBuyingPrice").text(data.GramGold.BuyingPrice);
+            $(".currency #gramGoldSellingPrice").text(data.GramGold.SellingPrice);
+            $(".currency #gramGoldUpdateTime").text(data.GramGold.UpdateTime);
+        } else {
+            $(".currency section").hide();
+            $(".currency .message").text("Bir hata oluştu..");
+        }
+        $('.currency section').removeClass('loading');
+    });
+});
